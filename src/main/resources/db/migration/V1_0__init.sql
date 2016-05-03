5cc7eda170194adb9244c6edfff5ca8b688f9e52
Robert Hunt <roberthunt87@gmail.com>
2016-05-04 00:18:20 +0100

    - remove duplicated column definition

--- src/main/resources/db/migration/V1_0__init.sql
+++ src/main/resources/db/migration/V1_0__init.sql
@@ -271,7 +271,6 @@ CREATE TABLE IF NOT EXISTS `mv_votes` (
 CREATE TABLE IF NOT EXISTS `mv_statuses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(255) NOT NULL,
-  `status` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
